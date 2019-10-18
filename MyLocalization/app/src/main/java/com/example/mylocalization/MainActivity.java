56604500f398149c403e1cf0c40d9cf54144b32e
alingali <56462832+alingali@users.noreply.github.com>
2019-10-18 16:37:54 +0700

    Add files via upload

---
+++ MyLocalization/app/src/main/java/com/example/mylocalization/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mylocalization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
